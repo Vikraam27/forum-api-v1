d0d3ef70146b646203bb22c01612c2fabe5e8217
Vikraam27 <ramvik36@gmail.com>
2022-05-16 12:09:48 +0700

    first commit

---
+++ migrations/1652677684397_create-table-threads.js
@@ -0,0 +1,30 @@
+exports.up = (pgm) => {
+  pgm.createTable('threads', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+      notNull: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    creator_username: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('treads');
+};
